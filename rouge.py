43ff14f7d0ece683d9d6415f73328560b471b7e4
4n3mone <bbangggo123@gmail.com>
2024-07-30 11:20:30 +0900

    fix

--- rouge.py
+++ rouge.py
@@ -1,10 +1,10 @@
 from korouge_score import rouge_scorer
 
-def ko_rouge(sentence1:str, sentence2:str)->dict:
+def ko_rouge(sentence1: str, sentence2: str) -> dict:
     #sentence1 : reference sentence
     #sentence2 : summary sentence
     scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL", "rougeLsum"])
     result = scorer.score(sentence1, sentence2)
-    return result
     
-
+    # score(fmeasure)만 추출
+    return {k: v.fmeasure for k, v in result.items()}
